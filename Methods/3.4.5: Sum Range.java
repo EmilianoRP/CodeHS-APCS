723f014529d5c1b545a0dca47c01268cac1266ca
EmilianoRP <78526232+EmilianoRP@users.noreply.github.com>
2021-02-03 22:18:50 -0800

    Update 3.4.5: Sum Range.java

--- Methods/3.4.5: Sum Range.java
+++ Methods/3.4.5: Sum Range.java
@@ -1,19 +1,11 @@
 public int sumFrom(int a, int b)
-	{
-
-int result = 0;
-       if(a>b) //swap them
-       {
-          result=a;
-          a=b;
-          b=result;
-          result=0;
-       }
-        while (a <=b) {
-        result+=a;
-        a++; 
-  }
-  return result;
+{
+    int result = 0; // this states the inital value of result as we as defining it
+    
+    while ( a <= b) //so you dont go past what b is
+    {
+        result += a;//both of these work for adding 
+        a++;
+    }
+    return result;//just returning the result
 }
-
-
